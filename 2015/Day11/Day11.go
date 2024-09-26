62fa68e976c915748d654cf84bde5b0ddf6bf6b3
Cameron Black <CameronBlack51@hotmail.co.uk>
2024-09-26 14:40:38 +0100

    2015/2011 Complete

--- 2015/Day11/Day11.go
+++ 2015/Day11/Day11.go
@@ -6,21 +6,22 @@ import (
 )
 
 func main() {
-	input := "cqjxjnds"
+	// input := "cqjxjnds"
+	part2input := "cqjxxyzz"
 
-	fmt.Println(part1(input))
+	fmt.Println(part1(part2input))
 }
 
 func part1(input string) string {
 	var password []string
 	increment := true
 
-	for i := len(input); i > 0; i-- {
+	for i := len(input) - 1 ; i >= 0; i-- {
 		if increment {
 			if input[i] == 122 {
 				password = append(password, string(97))
 			} else {
-				password = append(password, string(input[i]))
+				password = append(password, string(input[i] + 1))
 				increment = false
 			}
 		} else {
@@ -28,7 +29,17 @@ func part1(input string) string {
 		}
 	}
 
-	return strings.Join(password, "")
+	for i, j := 0, len(password)-1; i < j; i, j = i+1, j-1 {
+		password[i], password[j] = password[j], password[i]
+	}
+
+	passwordStr := strings.Join(password, "")
+
+	if forbiddenLetterChecker(passwordStr) && threeStraightChecker(passwordStr) && twoDoubleChecker(passwordStr) {
+		return passwordStr
+	}
+
+	return part1(passwordStr)
 }
 
 func forbiddenLetterChecker(password string) bool {
@@ -44,6 +55,22 @@ func forbiddenLetterChecker(password string) bool {
 func threeStraightChecker(password string) bool {
 	for i := 0; i < len(password) - 2; i++ {
 		if password[i + 2] == password[i + 1] + 1 && password[i + 1] == password[i] + 1 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func twoDoubleChecker(password string) bool {
+	doubleCounter := 0
+
+	for i := 0; i < len(password)-1; i++ {
+		if password[i] == password[i+1] {
+			doubleCounter++
+			i++
+		}
+		if doubleCounter >= 2 {
 			return true
 		}
 	}
