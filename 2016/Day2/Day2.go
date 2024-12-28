66612ab1d0d0dd887a56b9e18ff60dee4a5498b1
Alkestian <cameronblack51@hotmail.co.uk>
2024-12-28 11:40:07 +0000

    2016 Day 2 done

--- 2016/Day2/Day2.go
+++ 2016/Day2/Day2.go
@@ -7,14 +7,6 @@ import (
 )
 
 func main() {
-	keypad := [][]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9},
-	}
-
-	coord := []int{1, 1}
-
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println("Error reading file")
@@ -26,8 +18,20 @@ func main() {
 	for scanner.Scan() {
 		instructions = append(instructions, scanner.Text())
 	}
-	// sampleInstructions := []string{"ULL", "RRDDD", "LURDL", "UUUUD"}
+	//instructions := []string{"ULL", "RRDDD", "LURDL", "UUUUD"}
 
+	fmt.Println("Pin code:", part1(instructions))
+	fmt.Println("Pin code:", string(part2(instructions)))
+}
+
+func part1(instructions []string) []int {
+	keypad := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	coord := []int{1, 1}
 	pinCode := []int{}
 
 	for _, line := range instructions {
@@ -54,5 +58,44 @@ func main() {
 		pinCode = append(pinCode, keypad[coord[0]][coord[1]])
 	}
 
-	fmt.Println("Pin code:", pinCode)
+	return pinCode
+}
+
+func part2(instructions []string) []rune {
+    keypad := [][]rune{
+        {0, 0, '1', 0, 0},
+        {0, '2', '3', '4', 0},
+        {'5', '6', '7', '8', '9'},
+        {0, 'A', 'B', 'C', 0},
+        {0, 0, 'D', 0, 0},
+    }
+
+	coord := []int{2, 0}
+	pinCode := []rune{}
+
+	for _, line := range instructions {
+		for _, c := range line {
+			switch c {
+			case 'U':
+				if coord[0] > 0 && keypad[coord[0]-1][coord[1]] != 0 {
+					coord[0]--
+				}
+			case 'D':
+				if coord[0] < 4 && keypad[coord[0]+1][coord[1]] != 0 {
+					coord[0]++
+				}
+			case 'L':
+				if coord[1] > 0 && keypad[coord[0]][coord[1]-1] != 0 {
+					coord[1]--
+				}
+			case 'R':
+				if coord[1] < 4 && keypad[coord[0]][coord[1]+1] != 0 {
+					coord[1]++
+				}
+			}
+		}
+		pinCode = append(pinCode, keypad[coord[0]][coord[1]])
+	}
+
+	return pinCode
 }
