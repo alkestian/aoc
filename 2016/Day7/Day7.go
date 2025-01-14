ee86b4422892a629ab48cbf05f36e19e444acfd1
Alkestian <cameronblack51@hotmail.co.uk>
2025-01-14 20:13:38 +0000

    Day 7 finally done

--- 2016/Day7/Day7.go
+++ 2016/Day7/Day7.go
@@ -22,6 +22,7 @@ func main() {
 	}
 	
 	fmt.Println("Part 1:", part1(lines))
+    fmt.Println("Part 2:", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -81,29 +82,26 @@ func part2(lines []string) int {
 
         for _, piece := range pieces {
             //check for aba
-            for i := 0; i < len(piece) - 3; i++ {
+            for i := 0; i <= len(piece) - 3; i++ {
                 segment := piece[i:i+3]
-                if segment[0] == segment[2] {
+                if segment[0] == segment[2] && segment[0] != segment[1] {
                     abas = append(abas, segment)
                 } 
             }
         }
 
-        for _, hypernet := range hypernets {
-            //Check for bab
-            match := false
-            for i := 0; i < len(hypernet) - 3; i++ {
-                if match {
+        match := false
+        for _, aba := range abas {
+            bab := string(aba[1]) + string(aba[0]) + string(aba[1]) // Generate corresponding BAB
+            for _, hypernet := range hypernets {
+                if strings.Contains(hypernet, bab) {
+                    match = true
                     break
                 }
-                segment := hypernet[i:i+3]
-                for _, aba := range abas {
-                     //flip aba, check against segment
-                     if {
-                        match = true
-                        break
-                     }
-                }
+            }
+            if match {
+                abaCounter++
+                break
             }
         }
     }
